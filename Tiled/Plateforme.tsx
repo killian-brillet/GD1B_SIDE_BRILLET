beaf9dd122e0294755abb173eed4b2bd44c0f2ce
killian-brillet <74669539+killian-brillet@users.noreply.github.com>
2021-03-28 16:05:05 +0200

    Version Finale/Rendue

---
+++ Tiled/Plateforme.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="platform" tilewidth="70" tileheight="70" tilecount="12" columns="6">
+ <image source="../assets/platform.png" width="420" height="140"/>
+</tileset>
---
+++ Tiled/essence.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="essence" tilewidth="22" tileheight="35" tilecount="1" columns="1">
+ <image source="../assets/essence.png" width="22" height="35"/>
+</tileset>
